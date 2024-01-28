093f110ca3d041b5cc2c13ef9ad19590a8ccdf12
ceejay06s <christianbalais06@gmail.com>
2024-01-28 18:36:07 +0800

    Apisocket

--- app/console/ApiSocket.php
+++ app/console/ApiSocket.php
@@ -8,6 +8,7 @@ include_once Library . 'Http.php';
 use Flame\Http;
 use Flame\WebSocket;
 
+
 class ApiSocket extends WebSocket
 {
     private $http;
@@ -26,18 +27,20 @@ class ApiSocket extends WebSocket
 
     function procResponse($response = [])
     {
+        $i = 0;
         if (!empty($response['data'])) {
             foreach ($response['data'] as $videoId => $records) {
+                $i++;
                 if ($records['roundCount'] > 0) {
-                    var_dump('VidId:' . $videoId);
-                    //var_dump('Rounds:' . $records['roundCount']);
+                    //var_dump('VidId:' . $videoId);
                     $prevResult = null;
                     $roundResult = [];
                     $lst = [];
+                    $resCount = [];
+                    $scoreboard = null;
                     foreach ($records['roundRes'] as $roundNumber => $rounds) {
+                        $i++;
                         if (!empty($rounds['timestamp'])) {
-
-                            //var_dump('RoundNumber:' . $roundNumber);
                             $init = array(
                                 't_video_id' => $videoId,
                                 'round_number' => $roundNumber,
@@ -53,22 +56,26 @@ class ApiSocket extends WebSocket
                                 'create_date' => "NOW()",
                                 'update_date' => "NOW()",
                             );
-
+                            $pair_flg_desc = NULL;
                             if ($init['player_point'] > $init['banker_point']) {
                                 $init['player_win_cnt'] = 1; // Player is higher
                                 $roundResult['round_res'] = "P";
                                 $roundResult['is_tie'] = 0;
                                 switch ($init['pair_flg']) {
                                     case 1:
+                                        $pair_flg_desc = "庄对";
                                         $roundResult['symbol'] = "P2";
                                         break;
                                     case 2:
+                                        $pair_flg_desc = "闲对";
                                         $roundResult['symbol'] = "P2";
                                         break;
                                     case 3:
+                                        $pair_flg_desc = "庄闲对";
                                         $roundResult['symbol'] = "P4";
                                         break;
                                     default:
+                                        $pair_flg_desc = "无对子";
                                         $roundResult['symbol'] = "P1";
                                 }
                             } elseif ($init['banker_point'] > $init['player_point']) {
@@ -77,15 +84,19 @@ class ApiSocket extends WebSocket
                                 $roundResult['is_tie'] = 0;
                                 switch ($init['pair_flg']) {
                                     case 1:
+                                        $pair_flg_desc = "庄对";
                                         $roundResult['symbol'] = "B2";
                                         break;
                                     case 2:
+                                        $pair_flg_desc = "闲对";
                                         $roundResult['symbol'] = "B2";
                                         break;
                                     case 3:
+                                        $pair_flg_desc = "庄闲对";
                                         $roundResult['symbol'] = "B4";
                                         break;
                                     default:
+                                        $pair_flg_desc = "无对子";
                                         $roundResult['symbol'] = "B1";
                                 }
                             } else {
@@ -94,27 +105,49 @@ class ApiSocket extends WebSocket
                                 $roundResult['is_tie'] = 1;
                                 switch ($init['pair_flg']) {
                                     case 1:
+                                        $pair_flg_desc = "庄对";
                                         $roundResult['symbol'] = "T1";
                                         break;
                                     case 2:
+                                        $pair_flg_desc = "闲对";
                                         $roundResult['symbol'] = "T3";
                                         break;
                                     case 3:
+                                        $pair_flg_desc = "庄闲对";
                                         $roundResult['symbol'] = "T3";
                                         break;
                                     default:
+                                        $pair_flg_desc = "无对子";
                                         $roundResult['symbol'] = "T1";
                                 }
                             }
                             //var_dump($roundResult);
                             $prevResult = $roundResult['round_res'];
                             $lst[] = $roundResult;
+                            $resCount[]  = array(
+                                "id" => hash('MD5', $i),
+                                "og_id" => $i,
+                                "rd_num" => $init['round_number'],
+                                "pl_point" => $init['player_point'],
+                                "bk_point" => $init['banker_point'],
+                                "card_dlt_no" => $init['card_dealt_no'],
+                                "pair_flg" => $init['pair_flg'],
+                                "pair_flg_desc" => $pair_flg_desc,
+                                "player_win_cnt" => $init['player_win_cnt'],
+                                "banker_win_cnt" => $init['banker_win_cnt'],
+                                "bk_pr_tie_cnt" => $init['banker_player_tie_cnt'],
+                                "winner_per_round" => $roundResult['round_res'],
+                                "og_time" => $init['origin_timestamp'],
+                                "converted_time" => $init['convert_timestamp']
+                            );
                         }
                     }
                     if (count($lst) > 0) {
-                        $res = $this->removeInitialConsecTies($lst);
-                        print_r('[Scoreboard]:' . print_r($res, true));
+                        $scoreboard = $this->removeInitialConsecTies($lst);
+                        print_r('[Scoreboard]:' . print_r($scoreboard, true));
                     }
+
+                    print_r('[ResultCount]:' . print_r($resCount, true));
                 }
             }
         }
